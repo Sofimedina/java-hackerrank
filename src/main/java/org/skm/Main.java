80e5a7a0a848ddda711857aa0f4a7bd160c91329
Sofia Medina <sofi.k.medina@gmail.com>
2023-12-14 01:12:16 -0300

    tag contenx extractor
    Signed-off-by: Sofia Medina <sofi.k.medina@gmail.com>

--- src/main/java/org/skm/Main.java
+++ src/main/java/org/skm/Main.java
@@ -1,7 +1,18 @@
 package org.skm;
 
+import java.io.*;
+import java.util.*;
+import java.text.*;
+import java.math.*;
+import java.util.regex.*;
+
 public class Main {
+
+
+
     public static void main(String[] args) {
-        System.out.println("Hello  fhjfkjf!");
+
+        TagContexExtractor.run();
+
     }
 }
